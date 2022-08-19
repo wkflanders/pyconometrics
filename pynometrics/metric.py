d0c096802fadd24d926d419dff56d19b2d31c8df
wkflanders <wflanders@haverford.edu>
2022-08-19 05:15:41 -0400

    Fixed volatility

--- pynometrics/metric.py
+++ pynometrics/metric.py
@@ -6,15 +6,15 @@ default_period = 50
 
 def volatility(x, **kwargs):
 
-    # Generates the volatility
+    # Generates the trailing volatility
 
     # x: Dataframe
-    # inputcol (str): Column from x that is being used to calculate the moving average
+    # inputcol (str): Column from x that is being used to calculate the volatility
     #                 Defaults to "Adj Close"
-    # outputcol (str): Column where the moving average is being returned to
-    #                 Defaults to "Sma_(period)"
-    # period (int): Lookback period (in days) for calculating the average
-    #                 Defaults to 50
+    # outputcol (str): Column where the volatility is being returned to
+    #                 Defaults to "Volatility_(period)"
+    # period (int): Lookback period (in days) that volatility is being calculated across
+    #                 Defaults to 50 days
 
     # Example: 
     # NDAQ_volatility = volatility(NASDAQ, period = 30)
@@ -23,34 +23,39 @@ def volatility(x, **kwargs):
     v = pd.DataFrame()
 
     period = kwargs.get("period", default_period)
+    volstring = "Volatility_"+str(period)
 
-    if(x.shape[1] > 1):
-        _inputcol = kwargs.get("inputcol", "Adj Close")
-        _x = x.loc[:, _inputcol]
+    is_df = isinstance(x, pd.DataFrame)
 
-        _outputcol = kwargs.get("outputcol", "Volatility")
-        v[_outputcol] = _x.rolling(window=period).std()
-        v = v.iloc[period: , :]
+    if(is_df):
+        if(x.shape[1] < 1):
+            raise IndexError("Number of columns must be greater than 1!")
+        elif(x.shape[1] > 1):
+            _inputcol = kwargs.get("inputcol", "Adj Close")
+            _x = x[[_inputcol]]
 
-        return v
-    elif(x.shape[1] < 1 ):
-        raise Exception("Number of columns cannot be less than 1")
+            _outputcol = kwargs.get("outputcol", volstring)
+            v[_outputcol] = _x.rolling(window=period).std().dropna(axis="index")*np.sqrt(period)
+
+            return v
+        else:
+            _outputcol = kwargs.get("outputcol", volstring)
+            v[_outputcol] = x.rolling(window=period).std().dropna(axis="index")*np.sqrt(period)
+
+            return v
     else:
-        _outputcol = kwargs.get("outputcol", "Volatility")
-        v[_outputcol] = x.rolling(window=period).std()
-        v = v.iloc[period: , :]
+        raise ValueError("Input must be a dataframe!")
 
-        return v
 
 def growth(x, **kwargs):
 
-    # Returns the 
+    # Returns the growth
 
     # x: Dataframe
-    # inputcol (str): Column from x that is being used to calculate the moving average
+    # inputcol (str): Column from x that is being used to calculate the growth
     #                 Defaults to "Adj Close"
-    # outputcol (str): Column where the moving average is being returned to
-    #                 Defaults to "Sma_(period)"
+    # outputcol (str): Column where the growth is being returned to
+    #                 Defaults to "Growth_(period)"
     # 
 
     # 
