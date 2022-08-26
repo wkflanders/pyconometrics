a9424a68b000601a762347417ed694b8512977ca
wkflanders <wflanders@haverford.edu>
2022-08-26 14:50:19 -0400

    Updated volatility and removed growth for now

--- pyconometrics/metric.py
+++ pyconometrics/metric.py
@@ -32,12 +32,15 @@ def volatility(x, **kwargs):
         elif(x.shape[1] > 1):
             _inputcol = kwargs.get("inputcol", "Adj Close")
             _x = x[[_inputcol]]
+            _x = _x.iloc[:,-period]
 
             _outputcol = kwargs.get("outputcol", volstring)
             v[_outputcol] = _x.rolling(window=period).std().dropna(axis="index")*np.sqrt(period)
 
             return v
         else:
+            x = x.iloc[:,-period]
+
             _outputcol = kwargs.get("outputcol", volstring)
             v[_outputcol] = x.rolling(window=period).std().dropna(axis="index")*np.sqrt(period)
 
@@ -46,37 +49,38 @@ def volatility(x, **kwargs):
         raise ValueError("Input must be a dataframe!")
 
 
-def growth(x, **kwargs):
 
-    # Returns the growth
+#WIP
+# def growth(x, initial, end, **kwargs):
 
-    # x: Dataframe
-    # inputcol (str): Column from x that is being used to calculate the growth
-    #                 Defaults to "Adj Close"
-    # outputcol (str): Column where the growth is being returned to
-    #                 Defaults to "Growth_(period)"
-    # 
+#     # Returns the growth
 
-    # 
+#     # x: Dataframe
+#     # inputcol (str): Column from x that is being used to calculate the growth
+#     #                 Defaults to "Adj Close"
+#     # outputcol (str): Column where the growth is being returned to
+#     #                  Defaults to "Growth_(period)"
+#     # initial (str): Initial date from which to calculate growth
+#     #                In YYYY-MM-DD format
+#     # end (str): End date from which to calculate growth
+#     #            In YYYY-MM-DD format
 
-    growth = pd.DataFrame()
+#     try:
+#         datetime.datetime.strptime(initial, '%Y-%m-%d')
+#         datetime.datetime.strptime(end, '%Y-%m-%d')
+#     except ValueError:
+#         raise ValueError("Initial and end dates must be in YYYY-MM-DD format!")
 
-    period = kwargs.get("period", default_period)
+#     growth = pd.DataFrame()
 
-    if(x.shape[1] > 1):
-        _inputcol = kwargs.get("inputcol", "Adj Close")
-        _x = x.loc[:, _inputcol]
+#     period = kwargs.get("period", default_period)
 
-        _outputcol = kwargs.get("outputcol", "Volatility")
-        growth[_outputcol] = _x.rolling(window=period).std()
-        growth = growth.iloc[period: , :]
+#     is_df = isinstance(pd.DataFrame)
 
-        return v
-    elif(x.shape[1] < 1 ):
-        raise Exception("Number of columns cannot be less than 1")
-    else:
-        _outputcol = kwargs.get("outputcol", "Volatility")
-        v[_outputcol] = x.rolling(window=period).std()
-        v = v.iloc[period: , :]
-
-        return v
+#     if(is_df):
+#         if(x.shape[1] < 1):
+#             raise IndexError("Number of columns must be greater than 1!")
+#         elif(x.shape[1] > 1):
+#             _inputcol = kwargs.get("inputcol", "Adj Close")
+#             _x = x[[_inputcol]]
+#             indx = _x.loc(_x['Date'].values == initial)
